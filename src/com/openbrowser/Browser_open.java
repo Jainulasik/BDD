ee5a71eef210e27ecbd5b1219da6dbc8f46adecf
jainulasik <s.jainulasik@gmail.com>
2024-04-17 11:14:45 +0530

    Test

---
+++ src/com/openbrowser/Browser_open.java
@@ -0,0 +1,10 @@
+package com.openbrowser;
+
+public class Browser_open {
+public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver", "D:\\Selenium_\\Driver\\chromedriver.exe");
+	
+Webdriver driver = new ChromeDriver();
+
+}
+}
